5e0851371d0c0cf51cdeffe044e31248499400a4
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-02-09 14:16:26 +0630

    fix eslint errors

--- src/modules/displayMovie.js
+++ src/modules/displayMovie.js
@@ -1,18 +1,21 @@
 import {
-    getData, getLike, getMovie, pushLike, pushComment,
-  } from './util.js';
-  
-  const displayData = async () => {
-    const home = document.querySelector('#home');
-    // fetching the movie data from the API
-    const dataArray = await getData();
-  
-    // looping through the array
-    dataArray.forEach((data, index) => {
-      // creating a new div element
-      const dataCard = document.createElement('div');
-      dataCard.classList.add('container');
-      dataCard.innerHTML = `<img src="${data.image.medium}" alt="${data.name}">
+  getMovie, getLike, postLike,
+} from './fetch.js';
+
+const displayMovie = async () => {
+  const home = document.querySelector('#home');
+  // fetching the likes from the API
+  const allLikes = await getLike();
+  // fetching the movies from the API
+  const dataArray = await getMovie();
+  // looping through the array
+  dataArray.forEach((data, index) => {
+    // filtering the like for the movie id
+    const likes = allLikes.filter((like) => like.item_id === data.id);
+    // creating a new div element
+    const dataCard = document.createElement('div');
+    dataCard.classList.add('container');
+    dataCard.innerHTML = `<img src="${data.image.medium}" alt="${data.name}">
       <div class="caption">
       <span class="title">${data.name}</span>
       <i class="fa-regular fa-heart"></i>
@@ -20,8 +23,16 @@ import {
       <p id="like">${likes.length > 0 ? likes[0].likes : 0} Likes</p>
       <button id="film${index}">Comments</button>
       <button>Reservations</button>`;
-      home.appendChild(dataCard);
-    });
-  };
-  
-  export default displayData;
+    home.appendChild(dataCard);
+
+    // posting likes on the home page
+    const likeBtn = dataCard.querySelector('.fa-heart');
+    likeBtn.onclick = () => {
+      const like = dataCard.querySelector('#like');
+      postLike(data.id);
+      like.innerHTML = `${likes[0].likes + 1} Likes`;
+    };
+  });
+};
+
+export default displayMovie;
--- src/modules/fetch.js
+++ src/modules/fetch.js
@@ -7,23 +7,23 @@ const getMovie = async () => {
 };
 
 const getLike = async () => {
-    const response = await fetch(`${involvementUrl}likes`);
-    const data = await response.json();
-    return data;
+  const response = await fetch(`${involvementUrl}likes`);
+  const data = await response.json();
+  return data;
 };
 
 const postLike = async (id) => {
-    await fetch(`${involvementUrl}likes/`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        item_id: id,
-      }),
-    });
-  };
+  await fetch(`${involvementUrl}likes/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      item_id: id,
+    }),
+  });
+};
 
 export {
-    getMovie, getLike, postLike,
+  getMovie, getLike, postLike,
 };
