406747a80bbfee42670da4fca4757b64fbd5ec0e
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-02-16 20:48:56 +0630

    implement contact page in index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,25 @@ import './style.css';
 import displayData from './modules/displayMovie.js';
 
 window.onload = () => {
+  // display home page
   displayData();
+
+  // changing pages
+  const contactBtn = document.querySelector('#contactBtn');
+  const homeBtn = document.querySelector('#homeBtn');
+  const contact = document.querySelector('#contact');
+  const home = document.querySelector('#home');
+  const counter = document.querySelector('#counter');
+  
+  homeBtn.onclick = () => {
+    home.style.display = 'grid';
+    contact.style.display = 'none';
+    counter.style.display = 'block';
+  }
+
+  contactBtn.onclick = () => {
+    contact.style.display = 'block';
+    home.style.display = 'none';
+    counter.style.display = 'none';
+  }
 };
